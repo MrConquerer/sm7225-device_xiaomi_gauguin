557931ff7a09f5c6e2d30ae10b4822e42d636d6b
MrConquerer <63918525+MrConquerer@users.noreply.github.com>
2024-02-06 02:18:59 +0530

    Update ThermalActivity.java

--- parts/src/org/lineageos/settings/thermal/ThermalActivity.java
+++ parts/src/org/lineageos/settings/thermal/ThermalActivity.java
@@ -1,1 +1,18 @@
+package org.lineageos.settings.thermal;
 
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class ThermalActivity extends AppCompatActivity {
+
+    private static final String TAG_THERMAL = "thermal";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thermal); 
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new ThermalSettingsFragment(), TAG_THERMAL).commit();
+    }
+}
