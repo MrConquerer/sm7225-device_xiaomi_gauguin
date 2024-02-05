064b90a5ddc22e0073fb176434ffa9610fa62612
MrConquerer <63918525+MrConquerer@users.noreply.github.com>
2024-02-05 23:52:19 +0530

    Update init.recovery.qcom.sh

--- recovery/root/init.recovery.qcom.sh
+++ recovery/root/init.recovery.qcom.sh
@@ -1,1 +1,8 @@
-
+#!/system/bin/sh
+# Make recovery enforcing
+setenforce 1
+# Bind mount /dev/block/mapper to /dev/block/bootdevice/by-name
+# For compatibility with mods like Magisk and OpenGapps
+# (vendor and system should be more than enough)
+mount -o bind /dev/block/mapper/system /dev/block/bootdevice/by-name/system
+mount -o bind /dev/block/mapper/vendor /dev/block/bootdevice/by-name/vendor
