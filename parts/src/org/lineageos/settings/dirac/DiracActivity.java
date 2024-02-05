c0acd5ba1d2b89d9d47ee54a202722f0f1e3f0cd
MrConquerer <63918525+MrConquerer@users.noreply.github.com>
2024-02-06 01:38:49 +0530

    Update DiracActivity.java

--- parts/src/org/lineageos/settings/dirac/DiracActivity.java
+++ parts/src/org/lineageos/settings/dirac/DiracActivity.java
@@ -1,1 +1,17 @@
+package org.lineageos.settings.dirac;
 
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+public class DiracActivity extends AppCompatActivity {
+
+    private static final String TAG_DIRAC = "dirac";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dirac); 
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new DiracSettingsFragment(), TAG_DIRAC).commit();
+    }
+}
