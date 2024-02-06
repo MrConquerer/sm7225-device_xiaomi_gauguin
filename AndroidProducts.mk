621c19be4bde553a0d6c304e5c569c898a6bc1c7
MrConquerer <63918525+MrConquerer@users.noreply.github.com>
2024-02-06 12:58:23 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,18 +1,22 @@
-# Inherit from the aosp_x86_64 product
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_x86_64.mk)
+# Inherit from the aosp_arm64 product
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_arm64.mk)
 
 # Override the device name and product name variables
 DEVICE := gauguin
-PRODUCT := carbon
+PRODUCT := carbon_gauguin
 
 # Override the product makefile path
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/$(PRODUCT)_$(DEVICE).mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/$(PRODUCT).mk
 
 # Override the common lunch choices
 COMMON_LUNCH_CHOICES := \
-    $(PRODUCT)_$(DEVICE)-user \
-    $(PRODUCT)_$(DEVICE)-userdebug \
-    $(PRODUCT)_$(DEVICE)-eng
+    $(PRODUCT)-user \
+    $(PRODUCT)-userdebug \
+    $(PRODUCT)-eng
 
-# Override any other configurations that are different from the aosp_x86_64 product
+# Override any other configurations that are different from the aosp_arm64 product
+
+
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ...
